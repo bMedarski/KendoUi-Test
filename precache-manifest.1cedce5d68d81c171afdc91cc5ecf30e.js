aaa29095a51ebb52df471b6c368aa3accfc4b5b3
bMedarski <bmedarski@gmail.com>
2020-05-04 20:16:37 +0300

    Updates

---
+++ precache-manifest.1cedce5d68d81c171afdc91cc5ecf30e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0318e260c014d965c5fefebf24ea2c4c",
+    "url": "/KendoUi-Test/index.html"
+  },
+  {
+    "revision": "3b44f41f0b22c666fbf1",
+    "url": "/KendoUi-Test/static/css/2.86d98a49.chunk.css"
+  },
+  {
+    "revision": "ceb810cfe14835364879",
+    "url": "/KendoUi-Test/static/css/main.02d64f4f.chunk.css"
+  },
+  {
+    "revision": "3b44f41f0b22c666fbf1",
+    "url": "/KendoUi-Test/static/js/2.401d9ef3.chunk.js"
+  },
+  {
+    "revision": "3ef76f0a45c060e2fec7c12129166bf5",
+    "url": "/KendoUi-Test/static/js/2.401d9ef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceb810cfe14835364879",
+    "url": "/KendoUi-Test/static/js/main.6c37e61a.chunk.js"
+  },
+  {
+    "revision": "6ab6b5c0c3199180ad80",
+    "url": "/KendoUi-Test/static/js/runtime-main.7dfe3598.js"
+  }
+]);
